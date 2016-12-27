ad3033650659633d65c16f97cef48d9da6a8e5b8
chayaturner <dietaholic@gmail.com>
2016-12-27 01:30:43 -0500

    Added functional main program

--- src/asgn04/Program.java
+++ src/asgn04/Program.java
@@ -1,11 +1,4 @@
-package main;
-
-import asgn04.DemocraticPopRepublicanElecDisplay;
-import asgn04.FavorDemocraticDisplay;
-import asgn04.FavorRepublicanDisplay;
-import asgn04.HonestDisplay;
-import asgn04.HonestElecFavorDemocraticPopDisplay;
-import asgn04.VotingData;
+package asgn04;
 
 public class Program {
 
@@ -32,27 +25,27 @@ public class Program {
 		elecRepVotes[0] = 15;
 		
 		//STATE 2
-		demVotes[1] = 5000;
-		repVotes[1] = 6000;
+		demVotes[1] = 60000;
+		repVotes[1] = 50000;
 		elecDemVotes[1] = 20;
 		elecRepVotes[1] = 0;
 		
 		//STATE 3
-		demVotes[2] = 5000;
-		repVotes[2] = 6000;
-		elecDemVotes[2] = 20;
-		elecRepVotes[2] = 0;
+		demVotes[2] = 15500;
+		repVotes[2] = 25000;
+		elecDemVotes[2] = 0;
+		elecRepVotes[2] = 30;
 		
 		//STATE 4
-		demVotes[3] = 5000;
-		repVotes[3] = 6000;
-		elecDemVotes[3] = 20;
+		demVotes[3] = 12000;
+		repVotes[3] = 8000;
+		elecDemVotes[3] = 30;
 		elecRepVotes[3] = 0;
 		
 		//STATE 5
-		demVotes[4] = 5000;
-		repVotes[4] = 6000;
-		elecDemVotes[4] = 20;
+		demVotes[4] = 50000;
+		repVotes[4] = 6850;
+		elecDemVotes[4] = 12;
 		elecRepVotes[4] = 0;
 		
 		//set the votes
@@ -62,34 +55,34 @@ public class Program {
 		//displays will automatically resend their updated messages:
 	
 		//STATE 1
-		demVotes[0] = 40000;
-		repVotes[0] = 10000;
+		demVotes[0] = 16000;
+		repVotes[0] = 400000;
 		elecDemVotes[0] = 0;
-		elecRepVotes[0] = 15;
+		elecRepVotes[0] = 13;
 		
 		//STATE 2
-		demVotes[1] = 20000;
-		repVotes[1] = 1000000;
-		elecDemVotes[1] = 10;
+		demVotes[1] = 17800;
+		repVotes[1] = 1000;
+		elecDemVotes[1] = 5;
 		elecRepVotes[1] = 0;
 		
 		//STATE 3
 		demVotes[2] = 5000;
-		repVotes[2] = 6000;
-		elecDemVotes[2] = 20;
-		elecRepVotes[2] = 0;
+		repVotes[2] = 6000000;
+		elecDemVotes[2] = 0;
+		elecRepVotes[2] = 30;
 				
 		//STATE 4
-		demVotes[3] = 5000;
-		repVotes[3] = 6000;
-		elecDemVotes[3] = 20;
+		demVotes[3] = 12000;
+		repVotes[3] = 12345;
+		elecDemVotes[3] = 30;
 		elecRepVotes[3] = 0;
 				
 		//STATE 5
-		demVotes[4] = 5000;
-		repVotes[4] = 6000;
-		elecDemVotes[4] = 20;
-		elecRepVotes[4] = 0;
+		demVotes[4] = 1200;
+		repVotes[4] = 60500;
+		elecDemVotes[4] = 0;
+		elecRepVotes[4] = 12;
 		
 		states.setVotes(demVotes, repVotes, elecDemVotes, elecRepVotes);
 	}
